c7aa3383eae7a2d1ddc94aae3e202f18b17f6a2d
Paul <pcherian10@gmail.com>
2018-05-23 15:00:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student_info|
       student_info.each do |key, value|
-        Student.new(key = value)
+        Student.new(key => value)
       end
     end
   end
